f549ee337ce0cacf068a1a3a243cb65285d73352
0chroma <21014+0chroma@users.noreply.github.com>
2010-03-07 18:26:58 -0500

    added twitter as a question source

--- qp/settings.py
+++ qp/settings.py
@@ -4,4 +4,5 @@ QUESTION_SOURCES = ( #List of RSS feeds that we pull questions from
     "http://answers.yahoo.com/rss/allq",
     "http://www.lucid-desktop.org/forum/feeds/latest/",
     "http://forums.eyeos.org/index.php?type=rss;action=.xml",
+    "http://search.twitter.com/search.atom?q=?",
 )
