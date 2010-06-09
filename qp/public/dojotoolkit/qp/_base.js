c388b1476c738c860bbedb3e4bfa53d2b942cc5c
0chroma <21014+0chroma@users.noreply.github.com>
2010-06-09 15:42:49 -0400

    got public stores setup on the client-side

--- qp/public/dojotoolkit/qp/_base.js
+++ qp/public/dojotoolkit/qp/_base.js
@@ -5,9 +5,16 @@ dojo.require("dijit.layout.ContentPane");
 dojo.require("dijit.form.Button");
 dojo.require("dojoc.sandbox.rounded.Rounded");
 dojo.require("dijit.form.TextBox");
+dojo.require("dojox.data.PersevereStore");
 
 var qp = {
+    questionStore: null,
+    answerStore: null,
     answerPrefill: "Enter your answer here...",
+    init: function(){
+        this.textStart();
+        this.createStores();
+    },
     textClear: function(){
         var a = dijit.byId('answerEntry')
 		if(a.getValue() == this.answerPrefill){
@@ -24,5 +31,9 @@ var qp = {
 		dijit.byId('answerEntry').setValue(this.answerPrefill);
 		dojo.connect(dijit.byId('answerEntry'), 'onFocus', qp, "textClear");
 		dojo.connect(dijit.byId('answerEntry'), 'onBlur', qp, "textFill");
-	}
+	},
+    createStores: function(){
+        qp.questionStore = new dojox.data.PersevereStore({target: "/Question"});
+        qp.answerStore = new dojox.data.PersevereStore({target: "/Answer"});
+    }
 }
